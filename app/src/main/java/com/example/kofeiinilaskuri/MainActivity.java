59acf393cd53b90452cd1bff6c15234dc9eb06e9
IlariHalonen <ilari.halonen@metropolia.fi>
2020-11-24 14:10:11 +0200

    Kahvilaskuri(toimii?)
    viissii en tiiä

--- app/src/main/java/com/example/kofeiinilaskuri/MainActivity.java
+++ app/src/main/java/com/example/kofeiinilaskuri/MainActivity.java
@@ -2,13 +2,74 @@ package com.example.kofeiinilaskuri;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private int kofeiini = 0;
+    private int juoma = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText kofeiininMaara = (EditText) findViewById(R.id.kofeiininMaara);
+        EditText juomanMaara = (EditText) findViewById(R.id.juomanMaara);
+        Button tallenna = (Button) findViewById(R.id.tallenna);
+        Button hae = (Button) findViewById(R.id.hae);
+        Button kahvi = (Button) findViewById(R.id.kahviNappi);
+
+
+        tallenna.setOnClickListener(v -> {
+            SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            kofeiini = Integer.parseInt(kofeiininMaara.getText().toString());
+            juoma = Integer.parseInt(juomanMaara.getText().toString());
+            editor.putInt("kofeiini", kofeiini);
+            editor.putInt("maara", juoma);
+            editor.apply();
+        });
+
+        hae.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+                kofeiini = sp.getInt("kofeiini", 0);
+                juoma = sp.getInt("maara", 0);
+                Update();
+            }
+        });
+
+        kahvi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                kahvia();
+
+            }
+        });
+
     }
+    private void Update(){
+        TextView kofeiiniYht = (TextView) findViewById(R.id.yhtKofeiini);
+        TextView juomaYht = (TextView) findViewById(R.id.yhtMaara);
+        kofeiiniYht.setText(String.valueOf(kofeiini));
+        juomaYht.setText(String.valueOf(juoma));
+    }
+    private void kahvia(){
+        Intent intent = new Intent(this, kahviLaskuri.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/kofeiinilaskuri/kahviLaskuri.java
@@ -0,0 +1,34 @@
+package com.example.kofeiinilaskuri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+public class kahviLaskuri extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kahvi_laskuri);
+
+        Intent intent = getIntent();
+
+        NumberPicker np = findViewById(R.id.numberPicker);
+        textView = findViewById(R.id.kahvilaskuri);
+
+        np.setMinValue(0);
+        np.setMaxValue(50);
+
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                textView.setText("Juotuja kuppeja: " + newVal);
+            }
+        });
+    }
+}
