921dcf673bbfcad0770a0e2e5a18cbcb7a176f82
niinimps <74964638+niinimps@users.noreply.github.com>
2020-12-09 23:33:00 +0200

    kommentteja lisätty etenkin info

--- app/src/main/java/com/example/kofeiinilaskuri/Info.java
+++ app/src/main/java/com/example/kofeiinilaskuri/Info.java
@@ -18,10 +18,10 @@ public class Info extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         web = findViewById(R.id.webView);
-        WebSettings webSettings = web.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        web.setWebViewClient(new Callback());
-        web.loadUrl("https://www.terveyskirjasto.fi/terveyskirjasto/tk.koti?p_artikkeli=dlk01123");
+        WebSettings webSettings = web.getSettings(); //creates web settings object
+        webSettings.setJavaScriptEnabled(true); //enables javascript using web settings object
+        web.setWebViewClient(new Callback()); //creating object for an inner class
+        web.loadUrl("https://www.terveyskirjasto.fi/terveyskirjasto/tk.koti?p_artikkeli=dlk01123"); //call the url using webview object
 
     }
 
@@ -29,6 +29,6 @@ public class Info extends AppCompatActivity {
         @Override
         public boolean shouldOverrideKeyEvent(WebView view, KeyEvent event) {
             return false;
-        }
+        } //to not overwrite, webview should be able to load a page
     }
 }
